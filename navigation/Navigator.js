f877dfda9ba07d905623a7d5ba4b1048a3f6534c
TahirReyaz <tahir200213@st.jmi.ac.in>
2021-10-27 20:03:30 +0530

    Added bottom tab navigator

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import GenreScreen from '../screens/GenreScreen';
 import DetailScreen from '../screens/DetailScreen';
 import ListScreen from '../screens/ListScreen';
 import FavsScreen from '../screens/FavsScreen';
-import Colors from '../constants/Colors'
+import Colors from '../constants/Colors';
+import { Platform } from 'react-native';
 
-const Navigator = createStackNavigator({
+const StackNavigator = createStackNavigator({
   Genres: {
     screen: GenreScreen,
     navigationOptions: {
@@ -14,9 +19,9 @@ const Navigator = createStackNavigator({
     }
   },
   List: ListScreen,
-  Favs: FavsScreen,
   Details: DetailScreen
-}, {
+}, 
+{
   defaultNavigationOptions: {
     headerStyle: {
       backgroundColor: Colors.primary
@@ -25,4 +30,37 @@ const Navigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(Navigator);
+const tabScreenConfig = {
+  Anime: {
+    screen: StackNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <Ionicons name='play-circle' size={25} color={tabInfo.tintColor} />
+      },
+      tabBarColor: Colors.primary
+    }
+  },
+  Favourites: {
+    screen: FavsScreen,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+      },
+      tabBarColor: Colors.secondary
+    }
+  }
+}
+
+const TabNavigator = 
+  Platform.OS === 'android' 
+  ? createMaterialBottomTabNavigator ( tabScreenConfig, {
+      activeColor: 'white',
+      shifting: true
+    })
+  : createBottomTabNavigator( tabScreenConfig, {
+      tabBarOptions: {
+        activeTintColor: Colors.secondary
+      }
+    })
+
+export default createAppContainer(TabNavigator);
